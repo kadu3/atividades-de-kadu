5b4ccba55f48ee179bdaa8a678822bb58dd13f7b
kadu3 <kaduzin889@gmail.com>
2025-09-08 10:37:56 -0300

    Create sql.php

---
+++ sql.php
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS catalogo;
+USE catalogo;
+
+CREATE TABLE IF NOT EXISTS produtos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    preco DECIMAL(10,2) NOT NULL,
+    estoque INT NOT NULL
+);
